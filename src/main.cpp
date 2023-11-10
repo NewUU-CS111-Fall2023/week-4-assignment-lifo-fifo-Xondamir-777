a1f80006981e726f000b44dd0741cb4cb1502696
Xondamir-777 <150456608+Xondamir-777@users.noreply.github.com>
2023-11-11 02:46:45 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,36 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Zokirjonov Xondamir
+ * Date: 8.11.2023
+ * Name: 
  */
 
 #include <iostream>
 #include "task_1.h"
+#include "task_2.h"
+#include "task_3.h"
+#include "task_5.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Task 1" << std::endl;
+    cout << "Task 1" << std::endl;
     // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    cout << "Task 2" << std::endl;
+    ex1 e;
+    vector<int> v= {3, 2, 1};
+    vector<pair<int, int>> a = e.sort(v);
+    ex2 ee;
+    cout << ee.check_ex2("{[()]}") << "\n" << endl;
+
+    cout << "Task 3" << std::endl;
+    Expression E;
+    cout << E.expression("8 9 + 1 7 - * ") << endl;
+
+    cout << "Task 4" << std::endl;
+    //main1 mm;
+
+    cout << "Task 5" << std::endl;
+    TowerOfHanoi toh;
+    toh.change(5, 1, 2, 3);
     return 0;
 }
